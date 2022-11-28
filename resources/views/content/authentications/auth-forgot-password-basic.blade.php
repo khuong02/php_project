04fc26402032a4fee1bad1f49f234153d21699c8
Long Trần <longtran@Longs-MacBook-Pro.local>
2022-11-28 22:06:07 +0700

    design mvc

--- resources/views/content/authentications/auth-forgot-password-basic.blade.php
+++ resources/views/content/authentications/auth-forgot-password-basic.blade.php
@@ -33,7 +33,7 @@
             <button class="btn btn-primary d-grid w-100">Send Reset Link</button>
           </form>
           <div class="text-center">
-            <a href="{{url('auth/login-basic')}}" class="d-flex align-items-center justify-content-center">
+            <a href="{{url('auth/login')}}" class="d-flex align-items-center justify-content-center">
               <i class="bx bx-chevron-left scaleX-n1-rtl bx-sm"></i>
               Back to login
             </a>
