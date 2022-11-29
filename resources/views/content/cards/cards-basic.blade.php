b16fb9bb2f4eb4a89a4d626f2d3327dff2a07ec5
Long Trần <longtran@Longs-MacBook-Pro.local>
2022-11-29 08:18:53 +0700

    design topic

--- resources/views/content/cards/cards-basic.blade.php
+++ resources/views/content/cards/cards-basic.blade.php
@@ -34,7 +34,7 @@
             <div class="dropdown">
               <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
               <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Detail</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-detail me-1"></i> Detail</a>
                 <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
                 <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
               </div>
