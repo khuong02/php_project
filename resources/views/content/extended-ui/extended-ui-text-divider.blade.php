e0047d60451c3208131f685a6a7ce0e0a568a086
Long Trần <longtran@Longs-MacBook-Pro.local>
2022-12-01 20:09:44 +0700

    add paginate question

--- resources/views/content/extended-ui/extended-ui-text-divider.blade.php
+++ resources/views/content/extended-ui/extended-ui-text-divider.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <h4 class="fw-bold py-3 mb-4">
-  <span class="text-muted fw-light">Extended UI /</span> Text Divider
+  <span class="text-muted fw-light">Questions Management /</span> Answers
 </h4>
 
 <div class="row">
